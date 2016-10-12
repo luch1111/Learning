f08fab535d2bb6776e826b9df1dae82c24ecddc5
Konstantin <luch1111@yandex.ru>
2016-10-12 16:56:02 +0300

    add properties

--- TestProject/src/main/java/com/lux/tpr/Initialization.java
+++ TestProject/src/main/java/com/lux/tpr/Initialization.java
@@ -1,6 +1,7 @@
 package com.lux.tpr;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -12,7 +13,10 @@ public class Initialization {
     @BeforeClass
     public void launchBrowser() {
 
-        driver = new FirefoxDriver();
+        //driver = new FirefoxDriver();
+        System.setProperty("webdriver.chrome.driver",PropertyHolder.getProperty("wevdriverPath_chrome"));
+        driver = new ChromeDriver();
+
     }
 
     @AfterClass
---
+++ TestProject/src/main/java/com/lux/tpr/PropertyHolder.java
@@ -0,0 +1,34 @@
+package com.lux.tpr;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.logging.StreamHandler;
+
+public class PropertyHolder {
+
+    private static Properties properties = new Properties();
+
+    static {
+        InputStream stream = null;
+        try {
+            stream = new FileInputStream("C:\\Work\\IdeaProjects\\rm.properties");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        try {
+            properties.load(stream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private PropertyHolder() {
+
+    }
+
+    public static String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+}
