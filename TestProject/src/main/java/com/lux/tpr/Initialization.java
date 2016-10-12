c4f5d54d73ef0420f2d21c70b4f7926be5949c3f
Konstantin <luch1111@yandex.ru>
2016-10-12 18:32:07 +0300

    changes in prop

--- TestProject/src/main/java/com/lux/tpr/Initialization.java
+++ TestProject/src/main/java/com/lux/tpr/Initialization.java
@@ -3,19 +3,27 @@ package com.lux.tpr;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Initialization {
 
     public WebDriver driver;
 
     @BeforeClass
-    public void launchBrowser() {
+    public void launchBrowser() throws MalformedURLException {
 
-        //driver = new FirefoxDriver();
-        System.setProperty("webdriver.chrome.driver",PropertyHolder.getProperty("webdriverPath"));
-        driver = new ChromeDriver();
+        if (PropertyHolder.getProperty("seleniumServer").equals("on")) {
+            driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), DesiredCapabilities.chrome());
+        } else {
+            System.setProperty("webdriver.chrome.driver",PropertyHolder.getProperty("webdriverPath"));
+            driver = new ChromeDriver();
+        }
 
     }
 
